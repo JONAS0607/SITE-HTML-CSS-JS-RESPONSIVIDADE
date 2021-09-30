a61deeb75b06d613c24b591db3468af4bdc698f2
Jonas Cabral <cabral.j@unoesc.edu.br>
2021-09-30 11:16:41 -0300

    Finalizando aula 5 e botão ativo faltou colocar o bold por  especifidade

--- js/script.js
+++ js/script.js
@@ -18,13 +18,13 @@ for (const link of links) {
 /**Mudar o header da página quando der scroll */
 const header = document.querySelector("#header");
 const navHeader = header.offsetHeight;
-window.addEventListener("scroll", () => {
+function changeHeader() {
   if (window.scrollY >= navHeader) {
     header.classList.add("scroll");
   } else {
     header.classList.remove("scroll");
   }
-});
+}
 
 /**Testimonial */
 const swiper = new Swiper(".swiper", {
@@ -34,6 +34,12 @@ const swiper = new Swiper(".swiper", {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 });
 
 //**scroll review */
@@ -54,14 +60,44 @@ scrollReveal.reveal(
   { interval: 100 }
 );
 
+/**Menu ativo conforme a seção visível na página */
+
+const sections = document.querySelectorAll("main section[id]");
+function activateMenu() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
+
 //**Botão voltar para o topo */
-
-const backToTopButton = document.querySelector(".back-to-top");
-
-window.addEventListener("scroll", () => {
+function backToTop() {
   if (window.scrollY >= 500) {
     backToTopButton.classList.add("show");
   } else {
     backToTopButton.classList.remove("show");
   }
+}
+
+const backToTopButton = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+  changeHeader();
+  backToTop();
+  activateMenu();
 });
